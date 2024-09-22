f8f957bd0897053956f0b2083f87148c7a286e3b
NumiBlue <thekindlyblog@gmail.com>
2024-09-22 11:02:59 -0400

    reframes the userModels.js file

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,13 +1,26 @@
-const mongoose = require ('mongoose');
+// models/User.js
+const mongoose = require('mongoose');
 
-//Define user schema
-const userSchema = new mongoose.Schema({
-    name: { type: String, required: true},
-    email: { type: String, required: true},
-    password: { type: String, required: true}
+// Create a schema for users
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
 });
 
-//create model from schema
-const User = mongoose.model( 'User', userSchema);
-
-module.exports = User;
+module.exports = mongoose.model('User', UserSchema);
